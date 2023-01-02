09dbc4a47fa192b823f25bb9f7a586f0980a8b92
jerome-muscat <jerome.muscat@laplateforme.io>
2023-01-02 19:00:14 +0100

    fichier script job pour aller plus loin

---
+++ jour01/job... pour aller plus loin/détécter_caractère.py
@@ -0,0 +1,6 @@
+test = "Ceci est un test"
+# Vérifier si le caractère se trouve dans la chaine de caractère
+if "e" in test:
+    print ("caractère trouvée")
+else:
+    print("caractère non trouvée")
