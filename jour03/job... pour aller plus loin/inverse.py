910a43e5f3dd897d494b2a48a0093259be5787b3
jerome-muscat <jerome.muscat@laplateforme.io>
2023-01-05 10:10:48 +0100

    script job pour aller plus loin

--- jour03/job... pour aller plus loin/inverse.py
+++ jour03/job... pour aller plus loin/inverse.py
@@ -1,3 +1,10 @@
-caractère = input("veuiller taper ce que vous voulez inverser : ")
-inverse = "".join(reversed(caractère))
-print(inverse)
+def inversecaractère(caractère):
+    ch=""
+    for i in range((len(caractère)) -1, -1, -1):
+        ch = ch + caractère[i]
+    return ch
+
+print(inversecaractère("nikana"))
+
+caractère=(input("veuiller taper ce que vous voulez inverser : "))
+print(inversecaractère(caractère))
